f06848590350e461031915f0b6db880545e8a1f6
lsobanty <67146934+lsobanty@users.noreply.github.com>
2020-09-03 20:45:19 +0200

    Add files via upload

---
+++ c4/ConsoleApp1/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Dynamic;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("wpisz liczbe do ktorej chcesz dodac 8");
+            string a = Console.ReadLine();
+            int b = int.Parse(a);
+            int c = b + 8;
+            Console.WriteLine("wynik to ");
+            Console.WriteLine(c);
+
+
+
+
+
+
+
+
+
+            Console.ReadKey();
+
+        }
+
+    }
+   
+}
