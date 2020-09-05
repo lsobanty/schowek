7a888c1aeb0057b3509d589ee0fa19891d8be0c5
lsobanty <67146934+lsobanty@users.noreply.github.com>
2020-09-05 21:09:45 +0200

    Add files via upload

---
+++ c6/c6/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace c6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Podaj szerokość: ");
+            string xs = Console.ReadLine();
+            Console.Write("Podaj wysokość ");
+            string ys = Console.ReadLine();                     //przykladowy program z kanalu o wszystkim
+
+            int x = int.Parse(xs);
+            int y = int.Parse(ys);
+            for (int i = 0; i < y; i++)
+            {
+                for (int j = 0; j < x; j++)
+                {
+                    Console.Write("*");
+                }
+                Console.WriteLine();
+            }
+            Console.ReadLine();
+        }
+    }
+}
