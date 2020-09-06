130f4fb0a1cff771a8c898f7de52bf73dafdd168
lsobanty <67146934+lsobanty@users.noreply.github.com>
2020-09-07 00:07:37 +0200

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace c5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string x = Console.ReadLine();
+            int y = int.Parse(x)+2;
+            Console.WriteLine(y);
+            x = y.ToString();
+            Console.WriteLine(x-"lubie placuszki");
+           
+            Console.ReadKey();
+
+        }
+    }
+}
---
+++ c7/c7/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace c7
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Random rnd = new Random();
+            int LosowaLiczba = rnd.Next(1, 11);
+            int MojaLiczba;
+            int ileRazy = 0;
+            Console.WriteLine("zgadnij liczbe od 1 do 10");
+            do
+            {
+                ileRazy++;
+                 Console.Write("podaj liczbe:");
+                 MojaLiczba = int.Parse(Console.ReadLine());
+                if (LosowaLiczba > MojaLiczba)
+                {
+                    Console.WriteLine("liczba jest wieksza");
+                }
+                else if (MojaLiczba > LosowaLiczba) 
+                {
+                    Console.WriteLine("liczba jest mniejsza");
+                }
+                else
+                {
+                    Console.WriteLine("WYGRALES! za "+ileRazy+ ". razem");
+                }
+            } while (LosowaLiczba != MojaLiczba);
+
+
+
+             Console.ReadKey();
+                
+            
+        }
+    }
+}
+
