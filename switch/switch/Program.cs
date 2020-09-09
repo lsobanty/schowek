2fd043a08b2f0e8f65222180ed004c1af1b68852
lsobanty <67146934+lsobanty@users.noreply.github.com>
2020-09-09 22:02:12 +0200

    Add files via upload

---
+++ switch/switch/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+
+namespace switch1
+{
+    class Program
+{
+    static void Main(string[] args)
+    {
+        int liczba;
+        liczba=int.Parse(Console.ReadLine());
+
+            //if (liczba == 1)
+            //{
+            //    Console.WriteLine("Jeden");
+            //}
+            //else if (liczba == 2)
+            //{
+            //    Console.WriteLine("dwa");
+            //}
+            //else if (liczba == 3)
+            //{
+            //    Console.WriteLine("trzy");
+            //}
+            //else
+            //{
+            //    Console.WriteLine("inna wartosc");
+            //}
+            switch (liczba)
+            {
+                case 1:
+                    Console.WriteLine("jeden");
+                    goto koniec;
+                case 2:
+                    Console.WriteLine("dwa");
+                    goto case 3;
+                case 3:
+                    Console.WriteLine("trzy");
+                    break;
+                default:
+                    Console.WriteLine("inna wartosc");
+                    break;
+                
+
+            }
+            Console.WriteLine("test");
+            koniec:
+            Console.WriteLine("koniec");
+            Console.ReadLine();
+    }
+}
+}
