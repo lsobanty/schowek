3667a31cd03ea59a60e8b508c7f6bc446d75f0cc
lsobanty <67146934+lsobanty@users.noreply.github.com>
2022-07-29 22:44:48 +0200

    Add files via upload

---
+++ mozliwypowrotD/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace mozliwypowrotD
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int fdas;
+            fdas = Int32.Parse(Console.ReadLine());
+            for (int i = 0; i < fdas; i++) 
+            {
+                Console.WriteLine("Hello luke, great to see you!");
+                string odpowiedz = Console.ReadLine();
+                if (odpowiedz == "hi" || odpowiedz == "czesc")
+                {
+                    Console.WriteLine("I didn't think you would've replied");
+                }
+                else
+                {
+                    Console.WriteLine("spierdalaj!");
+                }
+            }
+           
+        }
+       
+    }
+}
